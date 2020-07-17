ccc31197eae8120c6d0653c864bfb1a6b7e222da
tobilasinde <tobilasinde2012@yahoo.com>
2020-07-17 19:04:24 +0100

    wrote api

--- modules/auth.js
+++ modules/auth.js
@@ -18,51 +18,51 @@ auth.initializeStrategy = function(passport) {
             passwordField: 'password',
             passReqToCallback: true
         },
-        function(req, email, password, cb) {
-            auth.checkCredentials( email, password, cb ); //change auth to this
-        }));
-
         // function(req, email, password, cb) {
-        //     console.log('I am here');
-        //     models.User.findOne({
-        //         where: {
-        //             email: email
-        //         },
-        //         include: [
-        //             {
-        //               model: models.Role,
-        //             //   attributes: ['id', 'role_name']
-        //             },
-        //             {
-        //               model: models.Department,
-        //             //   attributes: ['id', 'dept_name']
-        //             },
-        //             {
-        //               model: models.CurrentBusiness,
-        //             //   attributes: ['id', 'current_business_name']
-        //             },
-        //         ]
-        //     }).then(function(user) {
-        //         if (!user) {
-                    
-        //             return cb(null, false, { message: "No user with that email address" });
-        //         }
-        //         if (!isValidPassword(user.password, password)) {
-        //             console.log('I am here invalid password');
-        //             return cb(null, false, { message: "Password is incorrect" });
-        //         }
-        //         var userinfo = user.get();
-        //         console.log('I am user role ' + userinfo.Role.role_name);
-        //         user.update({
-        //             last_login: Date.now()
-        //         })
-        //         return cb(null, userinfo);
-
-        //     }).catch(function(err) {
-        //         console.log("Error:", err);
-        //         return cb(null, false);
-        //     });
+        //     auth.checkCredentials( email, password, cb ); //change auth to this
         // }));
+
+        function(req, email, password, cb) {
+            console.log('I am here');
+            models.User.findOne({
+                where: {
+                    email: email
+                },
+                include: [
+                    {
+                      model: models.Role,
+                    //   attributes: ['id', 'role_name']
+                    },
+                    {
+                      model: models.Department,
+                    //   attributes: ['id', 'dept_name']
+                    },
+                    {
+                      model: models.CurrentBusiness,
+                    //   attributes: ['id', 'current_business_name']
+                    },
+                ]
+            }).then(function(user) {
+                if (!user) {
+                    
+                    return cb(null, false, { message: "No user with that email address" });
+                }
+                if (!isValidPassword(user.password, password)) {
+                    console.log('I am here invalid password');
+                    return cb(null, false, { message: "Password is incorrect" });
+                }
+                var userinfo = user.get();
+                console.log('I am user role ' + userinfo.Role.role_name);
+                user.update({
+                    last_login: Date.now()
+                })
+                return cb(null, userinfo);
+
+            }).catch(function(err) {
+                console.log("Error:", err);
+                return cb(null, false);
+            });
+        }));
 
     passport.serializeUser(function(user, cb) {
         cb(null, user.id);
