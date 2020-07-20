22375ccf514c69f18c5d108a95f2f82bd5c344cb
tobilasinde <tobilasinde2012@yahoo.com>
2020-07-20 19:25:34 +0100

    setting global variables

--- config/config.production.js
+++ config/config.production.js
@@ -3,44 +3,26 @@ var config = require('./config.global');
 config.env = 'production';
 config.hostname = process.env.DB_HOSTNAME;
 config.db = {
-    // database: process.env.DB,
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // host: process.env.DB_HOSTNAME,
-    // sequelizeParams: {
-    //     dialect: process.env.DB_DIALECT,
-    //     use_env_variable: process.env.DB_URL,
-    //     host: process.env.DB_HOSTNAME
-    // }
-    database: 'casemanager',
-    username: 'manifestUser',
-    password: 'manifestPassword',
-    host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
+    database: process.env.DB,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
     sequelizeParams: {
-        dialect: 'postgres',
-        host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
-        operatorsAliases: false
+        dialect: process.env.DB_DIALECT,
+        use_env_variable: process.env.DB_URL,
+        host: process.env.DB_HOSTNAME
     }
 }
 
 config.sessionDb = {
-    // database: process.env.DB,
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // host: process.env.DB_HOSTNAME,
-    // sequelizeParams: {
-    //     dialect: process.env.DB_DIALECT,
-    //     use_env_variable: process.env.DB_URL,
-    //     host: process.env.DB_HOSTNAME
-    // }
-    database: 'casemanager',
-    username: 'manifestUser',
-    password: 'manifestPassword',
-    host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
+    database: process.env.DB,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
     sequelizeParams: {
-        dialect: 'postgres',
-        host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
-        operatorsAliases: false
+        dialect: process.env.DB_DIALECT,
+        use_env_variable: process.env.DB_URL,
+        host: process.env.DB_HOSTNAME
     }
 }
 
