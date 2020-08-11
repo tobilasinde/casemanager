cb83fb4218fd60b0399829efc7d3d2521a7ba126
tobilasinde <tobilasinde2012@yahoo.com>
2020-08-11 17:22:09 +0100

    Done

--- public/assets/Routes.js
+++ public/assets/Routes.js
@@ -4,15 +4,15 @@ class Routes {
   }
 
   get root () {
-    // return 'https://tonycasemanager.herokuapp.com/';
+    return 'https://tonycasemanager.herokuapp.com/';
     // return 'https://d56db9add77f4f85b2d170b091d7493f.vfs.cloud9.us-east-1.amazonaws.com';
-    return 'http://localhost:3000/'; // 
+    // return 'http://localhost:3000/'; // 
   };
 
   get apiRoot () { 
-    // return 'https://tonycasemanager.herokuapp.com/api';
+    return 'https://tonycasemanager.herokuapp.com/api';
     // return 'https://d56db9add77f4f85b2d170b091d7493f.vfs.cloud9.us-east-1.amazonaws.com/api';
-    return 'http://localhost:3000/api'; // API Route
+    // return 'http://localhost:3000/api'; // API Route
   };
 }
 
